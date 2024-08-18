08557c1eb60d114e865502b131ddcdd2be0983bb
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-08-18 13:40:29 +0200

    main page

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-   <NuxtPage/>
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <Navigation />
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@ import { computed, ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import Navigation from '~/components/Navigation.vue';
 
+const router = useRouter()
+
 const buttons = [
   { value: 'main-page', title: 'Strona Główna' },
   { value: 'pol-1', title: 'Ekstraklasa' },
@@ -16,17 +18,30 @@ const buttons = [
 
 <template>
   <v-layout class="rounded rounded-md">
-    <Navigation/>
     <v-navigation-drawer>
       <v-list>
-        <v-list-item v-for="button in buttons" :key="button.value"  >
-          <v-btn>{{ button.title }}</v-btn>
+        <v-list-item v-for="button in buttons" :key="button.value">
+          <v-btn :to="'leagues/'+button.value">{{ button.title }}</v-btn>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      tutaj beda wyniki
+      <v-img src="/main.jpg" cover gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)" class="mt-15 text-center">
+        <div class="d-flex flex-column align-center justify-center h-50 mx-4">
+          <span class="text-h3 font-weight-bold text-white">
+            MATCH RESULTS APP
+          </span>
+
+          <p class="mt-2 text-white">
+            Stwórz własną ligę i typuj razem ze swoimi znajomymi!
+          </p>
+
+          <v-btn class="my-4" to="auth/login" variant="elevated" color="secondary">
+            Dołącz już teraz
+          </v-btn>
+        </div>
+      </v-img>
     </v-main>
   </v-layout>
 </template>
