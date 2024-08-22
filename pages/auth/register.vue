8e88a7487dcb87558f79402773ccaa3929cf4bee
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-08-22 12:19:49 +0200

    proper register view

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,11 +1,20 @@
 <template>
-  
-    <v-img src="/register-background.jpg" cover gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)"
-      class="text-center w-100 h-100">
-      <v-row justify="center">
-        <v-col cols="12" sm="12" md="6">
-          <div class=" flex-column align-center justify-center h-100 mx-2 py-5">
-            <form>
+  <v-layout class="d-flex align-center justify-center" style="height: 100vh;">
+    <v-img src="/grass.jpg" cover gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)" class="text-center w-100 h-100">
+      <v-row justify="center" align="center" class="h-100">
+        <v-col cols="12" sm="12" md="6" class="d-flex justify-center align-center">
+          <div class="flex-column align-center justify-center mx-2 py-5"
+            style="background-color: rgba(0, 0, 0, 0.5); width: 100%; max-width: 500px;">
+            <v-spacer />
+            <span class="text-h3 font-weight-bold text-white">
+              ZAREJESTRUJ SIĘ
+            </span>
+            
+            <v-btn color="secondary" prepend-icon="mdi-google">
+              Rejestracja przez Google
+            </v-btn>
+
+            <form class="mx-5 py-2">
               <v-text-field v-model="email" label="Adres Email"></v-text-field>
 
               <v-text-field v-model="nickName" label="Nick"></v-text-field>
@@ -27,6 +36,8 @@
         </v-col>
       </v-row>
     </v-img>
+  </v-layout>
+
 </template>
 
 <script lang="ts" setup>
