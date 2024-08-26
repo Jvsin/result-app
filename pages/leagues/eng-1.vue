703b8cee0c5a48d3e8f5dc03a902da85bb241cf3
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-08-26 14:58:54 +0200

    some info added displaying

--- pages/leagues/eng-1.vue
+++ pages/leagues/eng-1.vue
@@ -4,12 +4,17 @@
       <template v-slot:title>
         <v-row cols="12" align="center">
           <v-col cols="4" sm="1">
-            <v-img :src="leagueData.logo" alt="League Logo" class="mb-4" :width="100" :height="100"></v-img>
+            <v-img :src="leagueData.logo" alt="League Logo" class="mb-4" max-height="50"></v-img>
           </v-col>
           <v-col cols="8" sm="11" class="d-flex align-center">
-            <span class="text-h4">
-              {{ leagueData.name }}
-            </span>
+            <v-card>
+              <v-card-title class="text-h3 my-2">
+                {{ leagueData.name }}
+              </v-card-title>
+              <v-card-subtitle>
+                {{ countSeason(leagueData.season) }}
+              </v-card-subtitle>
+            </v-card>
           </v-col>
         </v-row>
       </template>
@@ -47,12 +52,11 @@
         <v-card elevation="16" class="py-1" v-for="(standing, index) in leagueData.standings[0]" :key="index">
           <v-row class="d-flex align-center">
             <v-col cols="1" class="d-flex justify-center align-center">
-              <p class="text-center">{{ standing.rank }}</p>
+              <div :style="{ color: setColor(standing.description) }" class="text-center">{{ standing.rank }}</div>
             </v-col>
 
             <v-col cols="1" class="justify-center">
-              <v-img max-height="30" :src="standing.team.logo" aspect-ratio="1/1"
-                ></v-img>
+              <v-img max-height="30" :src="standing.team.logo" aspect-ratio="1/1"></v-img>
             </v-col>
 
             <v-col cols="7" class="d-flex align-center">
@@ -91,4 +95,23 @@ onMounted(() => {
   leagueStore.fetchLeagueData(39, 2024); // ID for Premier League
   console.log(leagueData)
 });
+
+function setColor(place: string) {
+  switch (place) {
+    case "Champions League":
+      return 'green'
+    case "Europa League":
+      return "orange";
+    case "Conference League":
+      return "green";
+    case "Relegation":
+      return "red";
+  }
+}
+
+function countSeason(season: string) {
+  let year = Number(season)
+  let result = season + '/' + String(year + 1)
+  return result
+}
 </script>
