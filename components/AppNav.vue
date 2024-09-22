f8a0ab0b67c28b2e6153bba16a26a66bf3c26b12
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-09-22 22:11:13 +0200

    button display hf

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -6,9 +6,10 @@
     <v-btn :class="isSmAndDown ? '' : 'ml-2'" variant="text" color="primary" :active="false" to="/"
       text="Match Results App" class="d-flex align-center justify-center" />
 
-    <v-select v-if="!isSmAndDown" v-model="locale" class="mx-2 ml-auto" variant="plain" hide-details :items="languages"
-      dense style="max-width: 120px;" />
+    <v-spacer></v-spacer>
 
+    <!-- <v-select v-if="!isSmAndDown" v-model="locale" variant="plain" hide-details :items="languages" dense
+      style="max-width: 120px;" class="d-flex align-center justify-center" /> -->
   </v-app-bar>
 
   <v-navigation-drawer v-if="isMdAndDown" v-model="drawer">
@@ -17,8 +18,11 @@
         <v-btn @click="getRoute(button.id)">{{ button.title }}</v-btn>
       </v-list-item>
     </v-list>
-    <v-select v-if="isSmAndDown" v-model="locale" class="mx-2 my-4" variant="plain" hide-details :items="languages"
-      dense style="max-width: 120px;" />
+    <v-divider></v-divider>
+    <div class="language-selector">
+      <v-select v-model="locale" class="mx-2 my-4" variant="plain" hide-details :items="languages" dense
+        style="max-width: 120px;" />
+    </div>
   </v-navigation-drawer>
 
   <v-navigation-drawer v-if="isSmAndUp">
@@ -27,6 +31,10 @@
         <v-btn @click="getRoute(button.id)">{{ button.title }}</v-btn>
       </v-list-item>
     </v-list>
+    <div class="language-selector">
+      <v-select v-model="locale" class="mx-2 my-4" variant="plain" hide-details :items="languages" dense
+        style="max-width: 120px;" />
+    </div>
   </v-navigation-drawer>
 </template>
 
@@ -74,3 +82,14 @@ watch((mdAndUp, smAndDown) => {
   // console.log('isMdAndUp: ' + isMdAndUp.value)
 })
 </script>
+
+<style scoped>
+.language-selector {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
