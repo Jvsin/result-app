89fe4e84a010f032c577fd5b9cfa0225c3fb6b2b
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-10-13 14:23:35 +0200

    hot fix of responsive view of bet view

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,13 +1,12 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-main class="d-flex align-center justify-center" style="height: 100vh;">
+    <v-main class="d-flex align-center justify-center" style="height: 100vh; overflow: hidden;">
       <v-img src="/user_main.jpg" cover gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)"
         class="text-center w-100 h-100">
-
         <v-sheet class="d-flex justify-center flex-wrap text-center mx-auto my-10 px-4" elevation="4"
           style="background-color: rgba(0, 0, 0, 0.5); height: 90%; width: 90%;" rounded>
           <v-row class="d-flex align-center flex-wrap" style="height: 150px;">
-            <v-col cols="12" sm="4" md="6" class="d-flex justify-center justify-sm-start  align-center mb-2 mb-sm-0">
+            <v-col cols="12" sm="4" md="6" class="d-flex justify-center justify-sm-start align-center mb-2 mb-sm-0">
               <span class="text-h4 text-center text-sm-left px-5">{{ $t('user.welcome') + "Jasin!" }}</span>
             </v-col>
             <v-col cols="12" sm="8" md="6" class="d-flex justify-center justify-sm-end justify-md-end align-center">
@@ -18,12 +17,11 @@
               </v-tabs>
             </v-col>
           </v-row>
-
-          <v-container>
+          <v-container style="height: calc(100% - 150px); overflow-y: auto;">
             <v-tabs-window v-model="tab">
-              <v-tabs-window-item :value="1">
+              <v-tabs-window-item :value="0">
                 <v-row>
-                  <v-col cols="4">
+                  <v-col cols="12" md="4">
                     <v-hover>
                       <template v-slot:default="{ isHovering, props }">
                         <v-card class="mx-auto" max-width="400" v-bind="props" :color="isHovering ? 'grey' : undefined">
@@ -31,16 +29,14 @@
                             PREMIER LEAGUE
                           </v-card-title>
                           <v-img class="align-end text-white" height="200" src="/public/pl.png"></v-img>
-
                           <v-card-actions>
-                            <v-btn block border color="primary">Typuj</v-btn>
-
+                            <v-btn block border color="primary">{{ $t('user.betButton') }}</v-btn>
                           </v-card-actions>
                         </v-card>
                       </template>
                     </v-hover>
                   </v-col>
-                  <v-col cols="4">
+                  <v-col cols="12" md="4">
                     <v-hover>
                       <template v-slot:default="{ isHovering, props }">
                         <v-card class="mx-auto" max-width="400" v-bind="props" :color="isHovering ? 'grey' : undefined">
@@ -48,16 +44,14 @@
                             EKSTRAKLASA
                           </v-card-title>
                           <v-img class="align-end text-white" height="200" src="/public/ekstraklasa.png"></v-img>
-
                           <v-card-actions>
-                            <v-btn block border color="primary">Typuj</v-btn>
-
+                            <v-btn block border color="primary">{{ $t('user.betButton') }}</v-btn>
                           </v-card-actions>
                         </v-card>
                       </template>
                     </v-hover>
                   </v-col>
-                  <v-col cols="4">
+                  <v-col cols="12" md="4">
                     <v-hover>
                       <template v-slot:default="{ isHovering, props }">
                         <v-card class="mx-auto" max-width="400" v-bind="props" :color="isHovering ? 'grey' : undefined">
@@ -65,10 +59,8 @@
                             CHAMPIONS LEAGUE
                           </v-card-title>
                           <v-img class="align-end text-white" height="200" src="/public/ucl.png"></v-img>
-
                           <v-card-actions>
-                            <v-btn block border color="primary">Typuj</v-btn>
-
+                            <v-btn block border color="primary">{{ $t('user.betButton') }}</v-btn>
                           </v-card-actions>
                         </v-card>
                       </template>
@@ -76,16 +68,16 @@
                   </v-col>
                 </v-row>
               </v-tabs-window-item>
-              <v-tabs-window-item :value="2">
+              <v-tabs-window-item :value="1">
                 <v-container>
                   ligi
                 </v-container>
               </v-tabs-window-item>
-              <v-tabs-window-item :value="3">profil</v-tabs-window-item>
-
+              <v-tabs-window-item :value="2">
+                profil
+              </v-tabs-window-item>
             </v-tabs-window>
           </v-container>
-
         </v-sheet>
       </v-img>
     </v-main>
@@ -98,6 +90,14 @@ const tab = ref(0)
 
 </script>
 
-<style>
+<style scoped>
+.v-main {
+  height: 100vh;
+  overflow: hidden;
+}
 
+.v-container {
+  height: calc(100% - 150px);
+  overflow-y: auto;
+}
 </style>
