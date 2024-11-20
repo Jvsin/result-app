7ffc0edd454bc6bd4c82165b00f9c622efe95824
Piotr Jasiński <118774312+Jvsin@users.noreply.github.com>
2024-11-20 18:50:43 +0100

    Revert "016 cloud functions init"

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'resultapp',
-  location: 'europe-central2'
-};
-exports.connectorConfig = connectorConfig;
-
